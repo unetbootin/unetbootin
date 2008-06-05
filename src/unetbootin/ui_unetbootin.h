f74c43766ed99df13f9e783c046b45ce81011227
Geza Kovacs <geza0kovacs@gmail.com>
2008-06-04 23:14:23 -0700

    misc fixes to build system

--- src/unetbootin/ui_unetbootin.h
+++ src/unetbootin/ui_unetbootin.h
@@ -1,7 +1,7 @@
 /********************************************************************************
 ** Form generated from reading ui file 'unetbootin.ui'
 **
-** Created: Wed Jun 4 20:28:34 2008
+** Created: Wed Jun 4 23:04:43 2008
 **      by: Qt User Interface Compiler version 4.4.0
 **
 ** WARNING! All changes made in this file will be lost when recompiling ui file!
