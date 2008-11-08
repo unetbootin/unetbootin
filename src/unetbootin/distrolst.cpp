806814bd135e01fb43b2515fc94114961eb71ac0
Geza Kovacs <geza0kovacs@gmail.com>
2008-11-08 09:32:32 -0500

    added copyright years to source header notices as per debian standards

--- src/unetbootin/distrolst.cpp
+++ src/unetbootin/distrolst.cpp
@@ -1,6 +1,6 @@
 /*
 distrolst.cpp from UNetbootin <http://unetbootin.sourceforge.net>
-Copyright (C) Geza Kovacs <geza0kovacs@gmail.com>
+Copyright (C) 2007-2008 Geza Kovacs <geza0kovacs@gmail.com>
 
 This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 2 of the License, or (at your option) any later version.
 
