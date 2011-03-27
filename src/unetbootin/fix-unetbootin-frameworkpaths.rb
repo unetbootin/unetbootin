ded0760e4a0eebf9b821d8e9e3593fa664d0b91b
Geza Kovacs <gkovacs@mit.edu>
2011-03-27 00:40:43 -0400

    fixed typo

--- src/unetbootin/fix-unetbootin-frameworkpaths.rb
+++ src/unetbootin/fix-unetbootin-frameworkpaths.rb
@@ -4,4 +4,4 @@ libs = `otool -L ./unetbootin.app/Contents/MacOS/unetbootin`.split("\n")
 libs = libs[1..libs.length]
 libs = libs.map { |x| x.split()[0] }
 libs = libs.reject { |x| x[0..0] == '/' || x[0..0] == '@' }
-libs.each { |x| `install_name_tool -change #{x} @executable_path/../Resources/#{x} ./unetbootin.app/Contents/MacOS/unetbootin` }
+libs.each { |x| `install_name_tool -change #{x} @executable_path/../Frameworks/#{x} ./unetbootin.app/Contents/MacOS/unetbootin` }
