89e3b462b73f6b0240705fa1b0af1e149d08779a
Geza Kovacs <gkovacs@mit.edu>
2011-03-26 22:09:59 -0400

    added mke2fs for Mac OS X

---
+++ src/unetbootin/fix-unetbootin-frameworkpaths.rb
@@ -0,0 +1,7 @@
+#!/usr/bin/ruby
+
+libs = `otool -L ./unetbootin.app/Contents/MacOS/unetbootin`.split("\n")
+libs = libs[1..libs.length]
+libs = libs.map { |x| x.split()[0] }
+libs = libs.reject { |x| x[0..0] == '/' || x[0..0] == '@' }
+libs.each { |x| `install_name_tool -change #{x} @executable_path/../Resources/#{x} ./unetbootin.app/Contents/MacOS/unetbootin` }
