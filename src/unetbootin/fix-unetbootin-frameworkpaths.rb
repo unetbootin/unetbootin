787323b2a4595c50a400f1024ca141e4708c7b7d
Geza Kovacs <gkovacs@stanford.edu>
2016-05-21 04:43:39 -0700

    updated qt version bundled with mac osx version to 4.8.7 and build as dmg

--- src/unetbootin/fix-unetbootin-frameworkpaths.rb
+++ src/unetbootin/fix-unetbootin-frameworkpaths.rb
@@ -3,5 +3,5 @@
 libs = `otool -L ./unetbootin.app/Contents/MacOS/unetbootin`.split("\n")
 libs = libs[1..libs.length]
 libs = libs.map { |x| x.split()[0] }
-libs = libs.reject { |x| x[0..0] == '/' || x[0..0] == '@' }
-libs.each { |x| `install_name_tool -change #{x} @executable_path/../Frameworks/#{x} ./unetbootin.app/Contents/MacOS/unetbootin` }
+libs = libs.reject { |x| x.start_with?('/usr/lib') || x[0..0] == '@' }
+libs.each { |x| `install_name_tool -change #{x} @executable_path/../Frameworks/#{x.sub("/usr/local/Trolltech/Qt-4.8.7/lib/", "")} ./unetbootin.app/Contents/MacOS/unetbootin` }
---
+++ src/unetbootin/unetbootin-mac-dmg/readme.txt
@@ -0,0 +1,2 @@
+Please right-click unetbootin and click "Open".
+You may get a warning that the developer is unidentified; just click "Open"
