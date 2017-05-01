e1bb2616dba89676730603cb1fd450679765cc63
Geza Kovacs <gkovacs@stanford.edu>
2017-04-30 22:39:45 -0700

    revert back to http to avoid openssl issues

--- src/unetbootin/distrolst.cpp
+++ src/unetbootin/distrolst.cpp
@@ -1193,7 +1193,7 @@ if (nameDistro == "Zenwalk")
 
 if (nameDistro == "3CX")
 {
-    downloadfile(QString("https://www.3cx.com/downloads/3cxpbxiso/debian-3cx.iso"), isotmpf);
+    downloadfile(QString("http://unetbootin.sourceforge.net/distros/3CX/pbx_debian_x64.php"), isotmpf);
     extractiso(isotmpf);
 }
 
