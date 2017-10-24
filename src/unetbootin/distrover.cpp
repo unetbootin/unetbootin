4400a81158c0f3c66fc6da6cf9cedf54453794e2
Geza Kovacs <gkovacs@stanford.edu>
2017-10-24 01:59:43 -0700

    there is no i386 iso for ubuntu 17.10

--- src/unetbootin/distrover.cpp
+++ src/unetbootin/distrover.cpp
@@ -14,7 +14,7 @@ This program is distributed in the hope that it will be useful, but WITHOUT ANY 
 "16.04_NetInstall" << "16.04_NetInstall_x64" << "16.04_HdMedia" << "16.04_HdMedia_x64" << "16.04_Live" << "16.04_Live_x64" << \
 "16.10_NetInstall" << "16.10_NetInstall_x64" << "16.10_HdMedia" << "16.10_HdMedia_x64" << "16.10_Live" << "16.10_Live_x64" << \
 "17.04_NetInstall" << "17.04_NetInstall_x64" << "17.04_HdMedia" << "17.04_HdMedia_x64" << "17.04_Live" << "17.04_Live_x64" << \
-"17.10_NetInstall" << "17.10_NetInstall_x64" << "17.10_HdMedia" << "17.10_HdMedia_x64" << "17.10_Live" << "17.10_Live_x64" << \
+"17.10_NetInstall" << "17.10_NetInstall_x64" << "17.10_HdMedia" << "17.10_HdMedia_x64" << "17.10_Live_x64" << \
 "Daily_Live" << "Daily_Live_x64"
 #endif
 
